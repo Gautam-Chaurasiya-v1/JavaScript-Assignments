53be57511e8c6ad30b737d0dd8bea00f592d9219
gautamc-creator <gautam.c@consultadd.com>
2026-02-06 13:48:32 +0530

    basics done 12 and 16

--- js-basics/intermediate_arr_obj.js
+++ js-basics/intermediate_arr_obj.js
@@ -194,8 +194,202 @@ for (const [key,val] of Object.entries(inp)){
 if(flag)console.log(true)
 */
 
+// 12
+
+/*
+const inp = { a: [1, [2, [3]]], b: [4, [5]] }
+
+function depth(arr){
+    let level = 0
+    for(const ele of arr){
+        if(Array.isArray(ele)){
+           level = Math.max(depth(ele),level)
+        }
+    }
+    return level + 1
+}
 
 
-console.log(out)
+for(const ele in inp){
+    let deep = depth(inp[ele])
+    console.log(deep)
+    inp[ele] = inp[ele].flat(deep-1);
+}
+
+console.log(inp)
+*/
+
+// 13 
+
+/*
+const inp = { fruits: ["apple","apple","banana"], drinks: ["apple","tea"] }
+let count = 0
+let ans = ""
+const out = Object.values(inp).reduce((acc,ele) => {
+    
+    for(const word of ele){
+        acc[word] = acc[word] || 0
+        acc[word] += 1
+
+        if(acc[word] > count){
+            ans = word
+            count = acc[word]
+        }
+    }
+
+    return acc
+},{})
+*/
+
+// 14 
+
+/*
+const inp = { a: [1,2,3,4], b: [2,3,4], c: [3,4,5] }
+const inp1 = Object.values(inp)
+const out = []
+
+for(const ele of inp1[0]){
+    let flag = true;
+    for(let i=1;i<inp1.length;i++){
+        if(!(inp1[i].includes(ele))){
+            flag = false
+            break
+        }
+    }
+   if(flag) out.push(ele)
+}
+*/
+
+// 15 
+
+/*
+const inp ={ a: { x: 1, y: 2 } }
+const inp1  ={ a: { y: 3, z: 4 } }
+
+function isObject(val){
+    return typeof val === 'object' && val != null && !Array.isArray(val)
+}
+
+function merge(objA,objB){
+    
+    for(const ele in objA){
+        if(!(ele in objB)){
+             objB[ele] = objA[ele]
+        }
+        else{
+            if(isObject(objA[ele])) merge(objA[ele],objB[ele])
+        }
+    }
+
+}
+
+merge(inp,inp1)
+
+console.log(inp,inp1)
+*/
+
+// 16 
+
+/*
+const inp ={ user: { profile: { name: "Alice", age: 25 } } }
+
+function isObject(val){
+    return typeof val === 'object' && val != null && !Array.isArray(val)
+}
+
+const out = {}
+
+function objectDestructre(obj){
+     for(const ele in obj){
+        if(isObject(obj[ele])){
+            objectDestructre(obj[ele])
+        }
+        else{
+            out[ele] = obj[ele]
+        }
+     }
+}
+
+objectDestructre(inp)
+*/
+
+// 17
+
+/*
+const inp ={ a: 10, b: 50, c: 30, d: 40 }, N=2
+const out = Object.entries(inp)
+
+out.sort((a,b) => b[1]-a[1])
+*/
+
+
+
+// 18 
+
+/*
+const inp =[
+  { name: "Alice", age: 30 },
+  { name: "Bob", age: 25 },
+  { name: "Alice", age: 22 }
+]
+
+inp.sort( function (a,b){
+
+    let nameA = a.name
+    let nameB = b.name
+    let ageA = a.age
+    let ageB = b.age
+
+    if(nameA<nameB)return -1
+    else if(nameA > nameB)return 1
+    else {
+        if(ageA < ageB)return -1
+        else return 1
+    }
+})
+
+console.log(inp)
+*/
+
+
+// 19
+
+/*
+const expected = ["a","b","c"]
+const actual = ["b","c","d"]
+const out = {}
+
+for(const ele of expected){
+    if(!(actual.includes(ele))){
+        out.missing = out.missing || []
+        out.missing.push(ele)
+    }
+}
+
+
+for(const ele of actual){
+    if(!(expected.includes(ele))){
+        out.extra = out.extra || []
+        out.extra.push(ele)
+    }
+}
+    */
+
+// 20 
+
+/*
+const inp = { a: 10, b: 20 } , inp1 = { a: 5, c: 15 }
+
+
+for(const [key,val] of Object.entries(inp)){
+   if(!(key in inp1)){
+    inp1[key] = val
+   }
+}
+
+console.log(inp1)
+*/
+
+// console.log(out)
 
 
